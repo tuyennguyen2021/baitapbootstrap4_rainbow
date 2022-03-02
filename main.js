20b33d16d0060774b1769fa02357eb064649eb85
“tuyennguyen2021” <tuyennguyen.cntp@gmail.com>
2022-03-02 23:47:46 +0700

    upload by tuyen

---
+++ main.js
@@ -0,0 +1,27 @@
+const dropdownToggle = document.getElementById("my-dropdown-toggle");
+const dropdownMenu = document.getElementById("my-dropdown-target");
+
+dropdownToggle.onclick = function() {
+  dropdownMenu.classList.toggle("activeWhenClick");
+}
+
+const dropdownToggle = document.getElementById("my-dropdown-toggle2");
+const dropdownMenu = document.getElementById("my-dropdown-target2");
+
+dropdownToggle.onclick = function() {
+  dropdownMenu.classList.toggle("activeWhenClick");
+}
+
+const dropdownToggle = document.getElementById("my-dropdown-toggle3");
+const dropdownMenu = document.getElementById("my-dropdown-target3");
+
+dropdownToggle.onclick = function() {
+  dropdownMenu.classList.toggle("activeWhenClick");
+}
+
+const dropdownToggle = document.getElementById("my-dropdown-toggle4");
+const dropdownMenu = document.getElementById("my-dropdown-target4");
+
+dropdownToggle.onclick = function() {
+  dropdownMenu.classList.toggle("activeWhenClick");
+}
